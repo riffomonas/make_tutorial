74d0e87e9a2100e70121a4683ef297ac0afe67bb
Pat Schloss <pschloss@umich.edu>
2016-07-14 13:50:06 -0400

    Create report of kids names information

--- baby_name_analysis.bash
+++ baby_name_analysis.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-################################################################################
-#
-# Prepare annual baby name data
-#
-################################################################################
+# Prepare annual baby name data report.
+# Assumes:
+#		*	the user is connected to the internet
+#		* has R installed in their PATH
+#		* has rmarkdown package installed				
 
 # Depends on: website data at https://www.ssa.gov/oact/babynames/names.zip
 # Produces: slew of files named yob????.txt and a pdf file
@@ -29,3 +29,11 @@ R CMD BATCH code/interpolate_mortality.R
 #							code/get_total_and_living_name_counts.R
 #	Produces:		data/processed/total_and_living_name_counts.csv
 R CMD BATCH code/get_total_and_living_name_counts.R
+
+
+# Renders an Rmarkdown file that creates various plots  and
+# provides an entertaining color commentary
+# Depends on:	data/processed/total_and_living_name_counts.csv
+#							code/plot_functions.R
+# Produces:		family_report.html
+R -e "library(rmarkdown); render('family_report.Rmd')"
